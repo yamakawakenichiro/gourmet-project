05d294079b1cf8cf49fe5c50720e0ea411e0813d
yamakawakenichiro_local <nykenichiro@gmail.com>
2024-10-23 12:24:32 +0900

    db: DB設計のため、マイグレーションファイル作成

---
+++ database/migrations/2024_10_23_102001_create_shops_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            //制約（constraint）:userデータが消えたら、shopデータも消える
+            $table->foreignId('user_id')->constrained();
+            $table->string('name', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
---
+++ database/migrations/2024_10_23_102234_create_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('shop_id')->constrained();
+            $table->string('name', 50);
+            $table->string('image', 100)->nullable();
+            $table->integer('price');
+            $table->integer('eaten_count')->default(0);
+            $table->string('body', 200);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
