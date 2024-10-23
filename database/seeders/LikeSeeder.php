6d288ff8c4bd8942f5e078651a28f681fa505463
yamakawakenichiro_local <nykenichiro@gmail.com>
2024-10-23 18:18:14 +0900

    db: シーディングのため、シーダーファイル作成

---
+++ database/seeders/LikeSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LikeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('likes')->insert([
+            'user_id' => 1,
+            'menu_id' => 3,
+        ]);
+    }
+}
---
+++ database/seeders/ShopSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ShopSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('shops')->insert([
+            'user_id' => 1,
+            'name' => 'マクドナルド',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('shops')->insert([
+            'user_id' => 1,
+            'name' => '松屋',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('shops')->insert([
+            'user_id' => 2,
+            'name' => 'バーガーキング',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+}
