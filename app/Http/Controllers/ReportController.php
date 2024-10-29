2d4c393313ded4b559ec58ab0e385b6c0f8ae89f
yamakawakenichiro_local <nykenichiro@gmail.com>
2024-10-29 20:51:01 +0900

    feat: 報告メール送信機能追加のため　reportコントローラー修正

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,11 +6,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests\ReportRequest;
 
 use App\Models\Menu;
+use App\Models\User;
 use App\Models\Category;
 use App\Models\Report;
 
+use Illuminate\Mail\Mailable;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
+
 
 class ReportController extends Controller
 {
@@ -30,6 +34,29 @@ class ReportController extends Controller
         $report->menu_id = $menu->id;
         $report->fill($input)->save();
 
+        //メール情報を取得
+        $reporter = User::where('id', $report->user_id)->value('name');
+        $userName = User::where('id', $menu->user_id)->value('name');
+        $category = Category::find($report->category_id);
+        $created_at = now();
+
+        //メール送信
+        if ($report) {
+            $recipient = config('mail.mailers.smtp.username');
+            $subject = 'グルメモ報告通知';
+
+            Mail::send('emails.report', [
+                'reporter' => $reporter,
+                'userName' => $userName,
+                'report' => $report,
+                'categoryName' => $category->name,
+                'created_at' => $created_at,
+            ], function ($message) use ($recipient, $subject) {
+                $message->from(config('mail.from.address'), config('mail.from.name'))
+                    ->to($recipient)
+                    ->subject($subject);
+            });
+        }
         return redirect()->route('index');
     }
 }
